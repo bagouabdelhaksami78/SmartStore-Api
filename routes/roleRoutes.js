e861f451445b6cc4a5933c970685841e134ec8c4
salaheddine ziani <salaheddine.ziani1996@gmail.com>
2024-11-21 17:52:35 -0500

    mise a jour des routes par salah

--- routes/roleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createRole, getAllRoles } = require('../controllers/roleController');
-const router = express.Router();
-
-router.post('/', createRole);
-router.get('/', getAllRoles);
-
-module.exports = router;
